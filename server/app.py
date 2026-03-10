b61bd35899fdfbf90ae81f144151cf35908c624d
commit-panda <sjmanyarkiy@gmail.com>
2026-03-10 12:48:17 +0300

    added registration authentication class

--- server/app.py
+++ server/app.py
@@ -3,12 +3,38 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, session
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import User
+
+## Auth
+class Register(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            user = User(
+                name=data['name'],
+                dob=data['dob'],
+                email=data['email'],
+                national_id=data['national_id'],
+                phone_number=data['phone_number'],
+                user_category=data['user_category']
+            )
+            user.password_hash = data['password']
+            db.session.add(user)
+            db.session.commit()
+
+            session['user_id'] = user.id
+            return user.to_dict(), 201
+        except Exception as e:
+            return {'error': str(e)}, 422
+        
+
+
 
 
 # Views go here!
