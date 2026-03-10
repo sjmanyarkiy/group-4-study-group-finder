a4b54b73db78636dfa4e2ba3a0e48d6e2c59c2a7
commit-panda <sjmanyarkiy@gmail.com>
2026-03-10 23:08:50 +0300

    added login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,68 @@
-import React from 'react'
+import React from 'react';
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { useHistory } from 'react-router-dom';
 
-function Login() {
-  return <h2>Log In</h2>
+function Login( { onLogin }) {
+    const history = useHistory();
+
+    const formSchema = yup.object().shape({
+        email: yup.string().email("Invalid email").required("Must enter email"),
+        password: yup.string().required("Must enter password")
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema: formSchema,
+        onSubmit: (values) => {
+            fetch("/login", {
+                method: "POST",
+                headers: { "Content-Type" : "application/json" },
+                body: JSON.stringify(values),
+            }).then((res) => {
+                if (res.ok) {
+                    res.json().then((data) => {
+                        onLogin(data);
+                        history.push("/");
+                    });
+                } else {
+                    res.json().then((data) => alert(data.error))
+                }
+            });
+        },
+    });
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={formik.handleSubmit}>
+                <label htmlFor="email">Email Address</label>
+                <input 
+                    id = "email"
+                    name = "email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
+                <p>{formik.errors.email}</p>
+
+                <label htmlFor="password">Password</label>
+                <input 
+                    id = "password"
+                    name = "password"
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                />
+                <p>{formik.errors.password}</p>
+
+                <button type="submit">LogIn</button>
+            </form>
+        </div>
+    )
+
 }
 
 export default Login
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 function Navbar({ user, onLogout}){
     return (
         <nav>
