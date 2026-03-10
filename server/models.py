aef773be1f51fcfee8fd06250513be73b840ade2
commit-panda <sjmanyarkiy@gmail.com>
2026-03-10 11:56:57 +0300

    defined columns for user model

--- server/models.py
+++ server/models.py
@@ -1,6 +1,40 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 from config import db
 
 # Models go here!
+
+class User(db.model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    dob = db.Column(db.String)
+    email = db.Column(db.String)
+    national_id = db.Column(db.Integer)
+    phone_number = db.Column(db.Integer)
+    user_category = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+
+    @validates('user_category')
+
+    @validates('email')
+    def validate_email(self, key, value):
+        if '@' not in value:
+            raise ValueError('Invalid email address')
+        return value
+        
+
+
+    def to_dict(self):
+        return {
+            'id' : self.id,
+            'name' : self.name,
+            'dob' : self.dob,
+            'email' : self.email,
+            'national_id' : self.national_id,
+            'phone_number' : self.phone_number,
+            'user_category' : self.user_category
+        }
