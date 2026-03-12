14fdecdf1884044fa6a45d0468a17bd559e6a6c9
Sam-Safari <samuelemanman@gmail.com>
2026-03-12 22:09:25 +0300

    refactor(client): App -> Router + Home; use mock data

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
-function App() {
-  return <h1>Project Client</h1>;
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+
+const mockCourses = [
+  { courseId: 1, courseName: 'Intro to Algorithms' },
+  { courseId: 2, courseName: 'Databases 101' },
+];
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Home courses={mockCourses} />
+      </Route>
+    </Switch>
+  </Router>
+);
+
+export default App;
 }
+ 
 
 export default App;
