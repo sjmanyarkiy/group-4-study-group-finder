e5373cbc935a37f84f092bddaba70b8511e44643
commit-panda <sjmanyarkiy@gmail.com>
2026-03-13 02:43:26 +0300

    deploying to render webservice

--- server/config.py
+++ server/config.py
@@ -1,6 +1,5 @@
-# Standard library imports
+import os
 
-# Remote library imports
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -8,18 +7,13 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-# Local imports
-
-# Instantiate app, set attributes
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-# API key
-app.secret_key = b'supersecretkey123'
+app.secret_key = os.environ.get('SECRET_KEY', b'supersecretkey123')
 
-# Define metadata, instantiate db
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
@@ -27,8 +21,9 @@ db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
 db.init_app(app)
 
-# Instantiate REST API
 api = Api(app)
 
-# Instantiate CORS
-CORS(app)
+CORS(app, supports_credentials=True, origins=[
+    "http://localhost:3000",
+    os.environ.get('FRONTEND_URL', '')
+])
